4d245ba66405a77939392bafbfee2ab9c5611e95
Nathan <nvm7252@rit.edu>
2025-01-07 16:10:40 -0500

    finished hit() in sphere class

---
+++ RayTracingInOneWeekend_Part1/sphere.h
@@ -0,0 +1,48 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "ray.h"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+    {
+        // set b = -2h in original code to get simplifications for variables
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+
+        auto discriminant = h * h - a * c;
+        // check if hidden behind something
+        if (discriminant < 0)
+        {
+            return false;
+        }
+
+        auto sqrtd = std::sqrt(discriminant);
+        // find nearest root in acceptable range
+        auto root = (h - sqrtd) / a;
+        // root is not along ray
+        if (root <= ray_tmin || ray_tmax <= root)
+        {
+            return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - center) / radius;
+
+        // sphere was hit
+        return true;
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
